0f8c909856642a9bbfbea2b591dc835582f12a0c
Pavlo Valchevskyi <qsplweroiucvbnm541@gmail.com>
2023-09-26 17:43:47 +0300

    #

---
+++ src/lr2/Main.java
@@ -0,0 +1,107 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lr2;
+
+import java.util.Random;
+import lr2_house.House;
+
+/**
+ *
+ * @author Valchevskyi
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Main houses = new Main(5);
+        System.out.println(houses + "\n");
+        houses.taskA(2);
+        System.out.println();
+        houses.taskB(1, 4, 8);
+        System.out.println();
+        houses.taskC(50);
+        System.out.println();
+    }
+    
+    // ===========================================================================
+    // ===========================================================================
+    // ===========================================================================
+    
+    String[] streets = {"Shevchenka", "Chornovola", "Zernova", "Ivasiuka", "Franka"}; // Array streets (for generation array houses).
+    String line = "--------------------------------------------------------"; // Line for output information.
+    House[] houses;
+
+    
+    private double roundDouble(double number)  { // For round number to x,xx.
+        return Math.round(number * 100) / 100.0;
+    }
+
+    public Main(int countHouses) {
+        houses = new House[countHouses];
+        Random random = new Random();
+        for (int i = 0; i < countHouses; i++) {
+            houses[i] = new House(i + 1, roundDouble(random.nextDouble(99) + 1), random.nextInt(8) + 1, random.nextInt(3) + 1, streets[random.nextInt(streets.length)]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("All houses:\n").append(line).append("\n");
+        for (House house : houses) {
+            stringBuilder.append(house).append("\n").append(line).append("\n");
+        }
+        stringBuilder.append("Count houses: ").append(houses.length).append("\n").append(line).append("\n");
+        return stringBuilder.toString();
+    }
+
+    public void taskA(int countRooms) {
+        int counter = 0;
+        System.out.println("Task A: List houses with count rooms: " + countRooms + "\n" + line);
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i].getCountRooms() == countRooms) {
+                counter++;
+                System.out.println(houses[i] + "\n" + line);
+            }
+        }
+        outputCountHousesInTask(counter);
+        System.out.println(line + "\n");
+    }
+
+    public void taskB(int countRooms, int levelMin, int levelMax) {
+        int counter = 0;
+        System.out.println("Task B: List houses with count rooms: " + countRooms + " and level range: [" + levelMin + "; " + levelMax + "]\n" + line);
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i].getCountRooms() == countRooms && houses[i].getLevel() >= levelMin && houses[i].getLevel() <= levelMax) {
+                counter++;
+                System.out.println(houses[i] + "\n" + line);
+            }
+        }
+        outputCountHousesInTask(counter);
+        System.out.println(line + "\n");
+    }
+
+    public void taskC(double square) {
+        int counter = 0;
+        System.out.println("Task C: List houses where square more: " + square + "\n" + line);
+        for (int i = 0; i < houses.length; i++) {
+            if (houses[i].getSquare() > square) {
+                counter++;
+                System.out.println(houses[i] + "\n" + line);
+            }
+        }
+        outputCountHousesInTask(counter);
+        System.out.println(line + "\n");
+    }
+
+    private void outputCountHousesInTask(int counter) {
+        if (counter == 0) {
+            System.out.println("Not houses in this condition!");
+        } else {
+            System.out.println("Count houses: " + counter);
+        }
+    }
+}
---
+++ src/lr2_house/House.java
@@ -0,0 +1,86 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lr2_house;
+
+/**
+ *
+ * @author Valchevskyi
+ */
+public class House {
+    private static int counter = -1;
+    private int id;
+    private int number;
+    private double square;
+    private int level;
+    private int countRooms;
+    private String street;
+
+    public House(int number, double square, int level, int countRooms, String street) {
+        counter++;
+        this.id = counter;
+        this.number = number;
+        this.square = square;
+        this.level = level;
+        this.countRooms = countRooms;
+        this.street = street;
+    }
+
+    // Setters and getters.
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setSquare(double square) {
+        this.square = square;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setCountRooms(int countRooms) {
+        this.countRooms = countRooms;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public double getSquare() {
+        return square;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getCountRooms() {
+        return countRooms;
+    }
+
+    public String getStreet() {
+        return new String(street);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("Data house:\n");
+        stringBuilder.append("\tID: ").append(id).append("\n");
+        stringBuilder.append("\tNumber: ").append(number).append("\n");
+        stringBuilder.append("\tSquare: ").append(square).append("\n");
+        stringBuilder.append("\tLevel: ").append(level).append("\n");
+        stringBuilder.append("\tCount rooms: ").append(countRooms).append("\n");
+        stringBuilder.append("\tStreet: ").append(street);
+        return stringBuilder.toString();
+    }
+}
